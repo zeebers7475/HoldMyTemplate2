fd05ae87977799349dfa72df128ffde1426e6c18
zeebers7475 <zeebers7475@gmail.com>
2024-10-17 02:36:11 -0400

    Add Input Boxes

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -6,15 +6,37 @@ import Input from './components/input';
 import TextArea from './components/textArea';
 
 
+
 export default function Home() {
-  const [input, setInput] = useState(<Input />);
+  const [k, setK] = useState(1);
+  const [inputs, setInputs] = useState([{key: 'I' + 0, input: <Input />}]);
   const [textArea, setTextArea] = useState(<textArea />);
 
+  // function mapInputs() {
+  //   inputs.map((input) => {
+  //     console.log(input)
+  //   })
+  // }
+
+  function addKey() {
+    setK((prev) => prev + 1)
+    return k
+  }
+
+  function addInput() {
+    const newKey = addKey()
+    setInputs([...inputs, {key:'I' + newKey, input: <Input addKey={newKey} />}])
+  }
   
+  
+
+
+
+
   return (
     <div>
-      <Nav />
-      {input}
+      <Nav addInput={addInput}/>
+      {inputs.map((input) => <div id={input.key}>{input.input}</div>)}
       {textArea}
     </div>
   );
